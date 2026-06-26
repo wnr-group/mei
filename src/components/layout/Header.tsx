f4e4681947ed9273c8e244b4a4f21e1498cbc58b
Eshwar154 <eshwarpaygude@gmail.com>
2026-06-26 12:56:50 +0530

    feat: wire Header search button to SearchModal with focus restoration

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -15,7 +15,6 @@ export default function Header() {
   const searchButtonRef = useRef<HTMLButtonElement>(null);
 
   useEffect(() => {
-    // eslint-disable-next-line react-hooks/set-state-in-effect
     setMounted(true);
   }, []);
 
